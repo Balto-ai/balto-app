77bdf8935ddd7929782df310c73a8119fa6c8d30
chcx97 <xiongchristy@gmail.com>
2022-07-18 11:53:49 -0700

    finished navbar and included starred page

--- balto-ui/src/components/NavBar/NavBar.jsx
+++ balto-ui/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import "./NavBar.css"
+import {Link, useNavigate} from 'react-router-dom'
+import { BsLightningCharge, BsStar } from "react-icons/bs";
 
 export default function NavBar() {
+  const navigate = useNavigate();
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+      <div className='header'>
+        <div className='logo'>Balto</div>
+        <div className='navlinks-div'>
+          <NavLinks className="nav-link"/>
+        </div>
+        <div className='nav-btns'>
+          <button className='navbar-btns' onClick={() => navigate("/register")}>Sign Up</button>
+          <button id='login' className='navbar-btns' onClick={() => navigate("/login")}>Login</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function NavLinks(){
+  
+  return(
+    <div className='navlinks'>
+      <span className='nav-header'>
+        <Link className='links' to="/search"><BsLightningCharge/> Find A Dog</Link>
+        <Link className='links' to="/star"><BsStar/> Favorites</Link>
+      </span>
+    </div>
   )
 }
---
+++ balto-ui/src/components/StarredPage/StarredPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StarredPage() {
+  return (
+    <div>StarredPage</div>
+  )
+}
