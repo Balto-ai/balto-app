1216872888c9ed1ce3d7b6ddcda9da77fd44f709
Annesa Tran <annesat2020@gmail.com>
2022-07-18 11:30:55 -0700

    Initial frontend complete for login page

--- balto-ui/src/components/LoginForm/LoginForm.jsx
+++ balto-ui/src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,81 @@
 import React from 'react'
+import { Link, useNavigate } from "react-router-dom"
+import "./LoginForm.css"
 
 export default function LoginForm() {
+  
+  const navigate = useNavigate()
+  const [errors, setErrors] = React.useState({})
+  const [form, setForm] = React.useState( { email: "",
+                                            password: ""
+                                          })
+  
+  const handleOnInputChange = (evt) => {
+    // check if email is valid
+    if (evt.target.name === "email") {
+      if (evt.target.value.indexOf("@") === -1) {
+        setErrors((existingErrors) => ({ ...existingErrors, email: "Please enter a valid email"}))
+      } else {
+        setErrors((existingErrors) => ({ ...existingErrors, email: null }))
+      }
+    }
+    // update form state var with input value
+    setForm((existingForm) => ({ ...existingForm, [evt.target.name]: evt.target.value }))
+  }
+
+  const handleOnFormSubmit = (evt) => {
+    evt.preventDefault()
+    setErrors((existingErrors) => ({ ...existingErrors, form: null }))
+
+    // TODO: attempt to login user (using auth context function OR ApiClient)
+    // TODO: check for user type and navigate to appropriate page for the user type
+  }
+
   return (
-    <div>LoginForm</div>
+
+    <div className="login-form">
+
+      {/* login card form (left) */}
+      <div className="login-card">
+        <h2>Login</h2>
+        <form className="form">
+
+          {/* email input */}
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input className="form-input"
+              name="email"
+              type="email"
+              value={form.email}
+              onChange={handleOnInputChange}
+            />
+            {errors.email && <span className="error">{errors.email}</span>}
+          </div>
+
+          {/* password input */}
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input className="form-input"
+              name="password"
+              type="password"
+              value={form.password}
+              onChange={handleOnInputChange}
+            />
+            {errors.password && <span className="error">{errors.password}</span>}
+          </div>
+
+          {/* submit button */}
+          <button className="submit-login" onClick={handleOnFormSubmit}>Login</button>
+
+        </form>
+
+      </div>
+
+      {/* login image (right) */}
+      <div className="login-image">
+        <img src="" alt="Login Image" />
+      </div>
+
+    </div>
   )
 }
--- balto-ui/src/components/LoginPage/LoginPage.jsx
+++ balto-ui/src/components/LoginPage/LoginPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom"
+import LoginForm from "../LoginForm/LoginForm"
 
 export default function LoginPage() {
+
+  const navigate = useNavigate()
+
+  // create useEffect here, if user is logged in, navigate to the landing page OR the admin dashboard
+  
   return (
-    <p>LoginPage</p>
+    <LoginForm />
   )
 }
