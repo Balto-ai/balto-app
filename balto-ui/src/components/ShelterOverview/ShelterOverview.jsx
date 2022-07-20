2b6ad11aba8109834e3561e222de1457b0bc7570
Annesa Tran <annesat2020@gmail.com>
2022-07-20 15:03:35 -0700

    Minor changes to dog record cards

--- balto-ui/src/components/ShelterOverview/ShelterOverview.jsx
+++ balto-ui/src/components/ShelterOverview/ShelterOverview.jsx
@@ -30,11 +30,39 @@ export default function ShelterOverview() {
 }
 
 export function DogRecordRow( props ) {
+
+    function getAgeGroup(dob) {
+        const birthDate = new Date(dob)
+        const currentDate = new Date()
+        let age_time = currentDate - birthDate
+        let age_days = Math.floor(age_time / (1000 * 3600 * 24))
+        // following this source for age groupings:
+        // https://www.frontiersin.org/articles/10.3389/fvets.2021.643085/full#:~:text=An%20option%20here%20would%20be,11%20years%20as%20Late%2DSenior.
+        if (age_days <= 180) {
+            return "Puppy"
+        } else if (age_days > 180 && age_days <=365) {
+            return "Junior"
+        } else if (age_days > 365 && age_days <= 730) {
+            return "Young Adult"
+        } else if (age_days > 730 && age_days <= 2555) {
+            return "Mature Adult"
+        } else if (age_days > 2555 && age_days <= 4380) {
+            return "Senior"
+        } else {
+            return "Geriatric"
+        }
+    }
+
     return (
         <div className="dog-record-row">
-            <img className="dog-record-image" src={props.imageUrl} alt={`Image of ${props.name}`} />
-            <p>{props.name}</p>
-            <p>{props.breed}</p>
+            <div className="col-1">
+                <img className="dog-record-image" src={props.imageUrl} alt={`Image of ${props.name}`} />
+                <div className="basic-info">
+                    <p className="dog-name">{props.name}</p>
+                    <p className="dog-info">{props.breed} | {props.sex == "m" ? "Male" : "Female"} | {getAgeGroup(props.dob)}</p>
+                </div>
+            </div>
+            <p>{props.dateEntered}</p>
         </div>
     )
 }
