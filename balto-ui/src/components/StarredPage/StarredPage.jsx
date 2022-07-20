c752cb0ffa4ad8f43e76025822f3cc9a215cdc88
charlesxu27 <charlesxu27@gmail.com>
2022-07-20 10:37:25 -0700

    add age range function

--- balto-ui/src/components/StarredPage/StarredPage.jsx
+++ balto-ui/src/components/StarredPage/StarredPage.jsx
@@ -4,6 +4,31 @@ import DogCard from "../DogCard/DogCard"
 import { user_dog_pairings, dogs } from "../../data"
 
 export default function FavoritesPage() {
+
+  function getAgeGroup(dob) {
+    const birthDate = new Date(dob)
+    const currentDate = new Date()
+    let age_time = currentDate - birthDate
+    let age_days = Math.floor(age_time / (1000 * 3600 * 24))
+    // following this source for age groupings:
+    // https://www.frontiersin.org/articles/10.3389/fvets.2021.643085/full#:~:text=An%20option%20here%20would%20be,11%20years%20as%20Late%2DSenior.
+    if (age_days <= 180) {
+      return "Puppy"
+    } else if (age_days > 180 && age_days <=365) {
+      return "Junior"
+    } else if (age_days > 365 && age_days <= 730) {
+      return "Young Adult"
+    } else if (age_days > 730 && age_days <= 2555) {
+      return "Mature Adult"
+    } else if (age_days > 2555 && age_days <= 4380) {
+      return "Senior"
+    } else {
+      return "Geriatric"
+    }
+  }
+
+
+
   return (
     <div className='main-div'>
       <h1 className='title'>Favorited Dogs {user_dog_pairings.length}</h1>
@@ -20,7 +45,7 @@ export default function FavoritesPage() {
       </div>
       <div className='starred-grid'>
         {dogs.map((dog, idx) => {
-          return ( <DogCard className="cards" key={idx} imgUrl={dog.image_url} name={dog.name} breed={dog.breed} ageGroup={dog.dob}   />)
+          return (<DogCard className="cards" key={idx} imgUrl={dog.image_url} name={dog.name} breed={dog.breed} ageGroup={getAgeGroup(dog.dob)} />)
         })}
       </div>
     </div>
