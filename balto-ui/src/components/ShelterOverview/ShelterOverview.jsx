eaf32e46e549b619001b4f9c750e81751caf42c9
Annesa Tran <annesat2020@gmail.com>
2022-07-21 14:05:43 -0700

    Added searchbar input, dropdown sort, and turned  dog record rows into Card components

--- balto-ui/src/components/ShelterOverview/ShelterOverview.jsx
+++ balto-ui/src/components/ShelterOverview/ShelterOverview.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import moment from "moment"
 import { useDogRecordsContext } from '../../contexts/dog-records'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap/Button'
+import Dropdown from 'react-bootstrap/Dropdown'
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card'
 import './ShelterOverview.css'
 
 export default function ShelterOverview() {
 
     const { dogRecords, error, isLoading, getAgeGroup  } = useDogRecordsContext()
+
     const [sort, setSort] = React.useState("")
+    const [searchQuery, setSearchQuery] = React.useState("")
 
     // sorting functions
     function sortByNameAsc(a, b) {
@@ -38,48 +45,45 @@ export default function ShelterOverview() {
     }
 
     // sort dogRecords based on sort criteria
-    if (sort == "NAME_ASC") {
+    if (sort == "Name (A-Z)") {
         dogRecords.sort( sortByNameAsc )
-    } else if (sort == "NAME_DESC") {
+    } else if (sort == "Name (Z-A)") {
         dogRecords.sort( sortByNameDesc )
-    } else if (sort == "DATE_ENTERED_ASC") {
+    } else if (sort == "Date Entered (Newest)") {
         dogRecords.sort( sortByDateEnteredAsc )
-    } else if (sort == "DATE_ENTERED_DESC") {
+    } else if (sort == "Date Entered (Oldest)") {
         dogRecords.sort( sortByDateEnteredDesc )
-    } else if (sort == "CREATED_AT_ASC") {
+    } else if (sort == "Newest Addition") {
         dogRecords.sort( sortByCreatedAtAsc )
-    } else if (sort == "CREATED_AT_DESC") {
+    } else if (sort == "Oldest Addition") {
         dogRecords.sort( sortByCreatedAtDesc )
+    }
+
+    let dogRecordsToRender = [...dogRecords]
+    if (searchQuery !== "") {
+        dogRecordsToRender = dogRecordsToRender.filter(dogRecord => dogRecord.name.toLowerCase().includes(searchQuery))
     }
 
     return (
         <div className="shelter-overview">
+            
+            {/* the top bar where the user can search by name or sort */}
+            <div className="filter-sort-bar">
+                {/* search bar */}
+                <DogRecordSearch setSearchQuery={setSearchQuery} />
 
-            {/* dropdown used to sort dogs in the shelter */}
-            <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Sort By: Name
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-
-                <button className="dropdown-item" type="button" onClick={()=>{setSort("NAME_ASC")}}>Name (A-Z)</button>
-                <button className="dropdown-item" type="button" onClick={()=>{setSort("NAME_DESC")}}>Name (Z-A)</button>
-                
-                <button className="dropdown-item" type="button" onClick={()=>{setSort("DATE_ENTERED_ASC")}}>Date Entered (Newest)</button>
-                <button className="dropdown-item" type="button" onClick={()=>{setSort("DATE_ENTERED_DESC")}}>Date Entered (Oldest)</button>
-
-                <button className="dropdown-item" type="button" onClick={()=>{setSort("CREATED_AT_ASC")}}>Date Created (Newest)</button>
-                <button className="dropdown-item" type="button" onClick={()=>{setSort("CREATED_AT_DESC")}}>Date Created (Oldest)</button>
-
-            </div>
+                {/* sort by dropdown menu */}
+                <DogRecordDropdown sort={sort} setSort={setSort} className="dog-record-sort" />
             </div>
 
             {/* list of all the dogs in the shelter */}
             <div className="dog-record-feed">
                 {dogRecords.length <= 0 && !isLoading
-                ? <h2 className="empty-message">No dogs added yet.</h2>
-                : dogRecords.map((dogRecord, idx) => {
-                    return <DogRecordRow
+                ? <h2 className="empty-message">No dogs to display</h2>
+                // create a row for each dog record
+                : dogRecordsToRender.map((dogRecord, idx) => {
+                    if (dogRecord.name) {
+                        return <DogRecordRow
                             key={dogRecord.id}
                             rowNumber={idx + 1}
                             dogRecordId={dogRecord.id}
@@ -89,28 +93,51 @@ export default function ShelterOverview() {
                             breed={dogRecord.breed}
                             ageGroup={getAgeGroup(dogRecord.dob)}
                             dateEntered={dogRecord.date_entered}
-                            /> } )
+                            />
+                    } })
                 }
             </div>
         </div>
     )
 }
 
+// searchbar updates searchQuery var whenever the value is changed
+export function DogRecordSearch({ setSearchQuery }) {
+    return (
+        <Form.Group className="mb-3 dog-record-search">
+          <Form.Control placeholder="Search by name" onChange={(evt)=>setSearchQuery(evt.target.value)} />
+        </Form.Group>
+    )
+}
+
+// calls appropriate sort function on the dogRecords when a sort is chosen
+export function DogRecordDropdown({ sort, setSort }) {
+    return (
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">Sort By: {sort}</Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item onClick={()=>{setSort("Name (A-Z)")}}>Name (A-Z)</Dropdown.Item>
+                <Dropdown.Item onClick={()=>{setSort("Name (Z-A)")}}>Name (Z-A)</Dropdown.Item>
+                <Dropdown.Item onClick={()=>{setSort("Date Entered (Newest)")}}>Date Entered (Newest)</Dropdown.Item>
+                <Dropdown.Item onClick={()=>{setSort("Date Entered (Oldest)")}}>Date Entered (Oldest)</Dropdown.Item>
+                <Dropdown.Item onClick={()=>{setSort("Newest Addition")}}>Newest Addition</Dropdown.Item>
+                <Dropdown.Item onClick={()=>{setSort("Oldest Addition")}}>Oldest Addition</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+}
+
+// TODO: need to figure out formatting and exactly which props we want to show up here
 export function DogRecordRow( props ) {
     return (
-        <div className="dog-record-row">
-            {/* col-1 includes the row number, dog image, name, breed, sex, and age group */}
-            <div className="col-1">
-                <p className="dog-record-number">{props.rowNumber}</p>
-                <img className="dog-image" src={props.imageUrl} alt={`Image of ${props.name}`} />
-                <div className="basic-info">
-                    <p className="dog-name">{props.name}</p>
-                    <p className="dog-info">{props.breed} | {props.sex == "m" ? "Male" : "Female"} | {props.ageGroup}</p>
-                </div>
-            </div>
-            <div className="col-2">
-                <p className="date-entered">Entered on {moment(new Date(props.dateEntered)).format("MM/DD/YYYY")}</p>
-            </div>
-        </div>
+
+        <Card style={{ width: '100%', minWidth: '700px' }}>
+            <Card.Body>
+                <img className="dog-record-image" src={props.imageUrl} alt={`Image of ${props.name}`} />                    
+                <Card.Title>{props.name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{props.breed} | {props.sex == "m" ? "Male" : "Female"} | {props.ageGroup}</Card.Subtitle>
+                <Card.Text>Entered {moment(new Date(props.dateEntered)).format("MM/DD/YYYY")}</Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
