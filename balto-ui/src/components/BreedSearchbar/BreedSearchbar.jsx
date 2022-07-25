d79ccf9b97996ee0e1d14f292653aae5156c64a1
Annesa Tran <annesat2020@gmail.com>
2022-07-25 11:42:53 -0700

    Give access to selected filters as an array

--- balto-ui/src/components/BreedSearchbar/BreedSearchbar.jsx
+++ balto-ui/src/components/BreedSearchbar/BreedSearchbar.jsx
@@ -6,13 +6,15 @@ import './BreedSearchbar.css'
 export default function BreedSearchbar() {
 
   const [dogBreeds, setDogBreeds] = React.useState([])
-  // const [selectedBreeds, setSelectedBreeds] = React.useState([])
+  const [selectedBreeds, setSelectedBreeds] = React.useState([])
   const [error, setError] = React.useState(null)
 
+  // useEffect to get JSON object of dog breed names
   React.useEffect(() => {
     const fetchDogBreeds = async () => {
         const { data, error } = await ApiClient.fetchDogBreeds()
         if (data?.dogBreeds) {
+          // turn the JSON object into a usable array of dog breeds - ["Poodle", "Labrador", ...]
           setDogBreeds(Object.values({ ...data.dogBreeds }))
           setError(null)
         }
@@ -21,12 +23,24 @@ export default function BreedSearchbar() {
     fetchDogBreeds()
     }, [])
 
-
+  // supplies options that will be shown in the returned Select element
   const dogBreedOptions = dogBreeds.map((dogBreed) => 
     {return {value:dogBreed, label:dogBreed}}
   )
+
+  // updates selectedBreeds state var
+  const handleSelectionChange = (evt) => {
+    setSelectedBreeds( evt.map(breed => breed.value) )
+  }
   
   return (
-    <Select options={dogBreedOptions} />
+    <Select
+      options={dogBreedOptions}
+      placeholder="Search by breed"
+      value={dogBreedOptions.filter((breedSelector) => selectedBreeds.includes(breedSelector.value))}
+      onChange={handleSelectionChange}
+      // isMulti allows multiple dog breeds to be selected
+      isMulti
+     />
   )
 }
