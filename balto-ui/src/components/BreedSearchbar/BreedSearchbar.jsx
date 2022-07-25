695be8f74930dc97ee186cee408cc2404a4f69f9
Annesa Tran <annesat2020@gmail.com>
2022-07-25 10:56:39 -0700

    Updated BreedSearchBar to call ApiClient to get dog breed names

--- balto-ui/src/components/BreedSearchbar/BreedSearchbar.jsx
+++ balto-ui/src/components/BreedSearchbar/BreedSearchbar.jsx
@@ -6,16 +6,24 @@ import Form from 'react-bootstrap/Form'
 import Dropdown from 'react-bootstrap/Dropdown'
 import './BreedSearchbar.css'
 
-const dogBreedsJsonData = require('../../data/breed_info.json')
-
 export default function BreedSearchbar() {
 
-  // const [dogBreeds, setDogBreeds] = React.useState({})
-  // const [breedQuery, setBreedQuery] = React.useState()
+  const [dogBreeds, setDogBreeds] = React.useState([])
   // const [selectedBreeds, setSelectedBreeds] = React.useState([])
-  // const [error, setError] = React.useState(null)
+  const [error, setError] = React.useState(null)
 
-  const dogBreeds = Object.keys(dogBreedsJsonData.dog_breeds)
+  React.useEffect(() => {
+    const fetchDogBreeds = async () => {
+        const { data, error } = await ApiClient.fetchDogBreeds()
+        if (data?.dogBreeds) {
+          setDogBreeds(Object.values({ ...data.dogBreeds }))
+          setError(null)
+        }
+        if (error) setError(error)
+    }
+    fetchDogBreeds()
+    }, [])
+
 
   const dogBreedOptions = dogBreeds.map((dogBreed) => 
     {return {value:dogBreed, label:dogBreed}}
@@ -24,34 +32,4 @@ export default function BreedSearchbar() {
   return (
     <Select options={dogBreedOptions} />
   )
-  
-  // return (
-  //   <Dropdown>
-  //     <Dropdown.Toggle
-  //       as="input"
-  //       placeholder="Search for a breed..."
-  //         onChange={(evt)=>setBreedQuery(evt.target.value)}
-  //     />
-
-  //     <Dropdown.Menu>        
-  //       {dogBreeds.map((breed) => {
-  //           return <Dropdown.Item key={breed}>{breed}</Dropdown.Item>
-  //         })
-  //       }
-  //     </Dropdown.Menu>
-  //   </Dropdown>
-
-  // )
 }
-
-  // React.useEffect(() => {
-  //   const fetchDogBreeds = async () => {
-  //       const { data, error } = await ApiClient.fetchDogBreeds()
-  //       if (data?.message) {
-  //         setDogBreeds({ ...data.message })
-  //         setError(null)
-  //       }
-  //       if (error) setError(error)
-  //   }
-  //   fetchDogBreeds()
-  //   }, [])
