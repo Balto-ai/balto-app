7b51aede941c3917730679992b84b133e131575d
Annesa Tran <annesat2020@gmail.com>
2022-07-25 21:01:11 -0700

    Updated login to use Bootstrap components

--- balto-ui/src/components/LoginForm/LoginForm.jsx
+++ balto-ui/src/components/LoginForm/LoginForm.jsx
@@ -1,95 +1,84 @@
 import React from 'react'
 import { Link, useNavigate } from "react-router-dom"
 import { useAuthContext } from '../../contexts/auth'
+import Form from 'react-bootstrap/Form'
+import FloatingLabel from 'react-bootstrap/esm/FloatingLabel'
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
+import { BsX } from "react-icons/bs"
 import "./LoginForm.css"
 
 export default function LoginForm() {
   
   const navigate = useNavigate()
-  const [errors, setErrors] = React.useState({})
   const [form, setForm] = React.useState( { email: "",
                                             password: ""
                                           })
-  
-  const { user, loginUser, validateEmail } = useAuthContext()
-  
+  const [isValidated, setIsValidated] = React.useState(false)
+
+  const { user, loginUser, error } = useAuthContext()
+
+
   const handleOnInputChange = (evt) => {
-    // check if email is valid
-    if (evt.target.name === "email") {
-      if (!validateEmail(evt.target.value)) {
-        setErrors((existingErrors) => ({ ...existingErrors, email: "Please enter a valid email"}))
-      } else {
-        setErrors((existingErrors) => ({ ...existingErrors, email: null }))
-      }
-    }
     // update form state var with input value
     setForm((existingForm) => ({ ...existingForm, [evt.target.name]: evt.target.value }))
   }
 
   const handleOnFormSubmit = async (evt) => {
     evt.preventDefault()
-    setErrors((existingErrors) => ({ ...existingErrors, form: null }))
+    setIsValidated(true)
+    const loginForm = evt.currentTarget
+    if (loginForm.checkValidity() === false) {
+      evt.stopPropagation()
+    } else {
+      await loginUser(form)
 
-    // TODO: attempt to login user (using auth context function OR ApiClient)
-    // TODO: check for user type and navigate to appropriate page for the user type
-
-    await loginUser(form)
-    if (user?.email) {
-      if (user?.shelterId) {
-        navigate("/admin-dashboard")
-      } else {
-        navigate("/")
+      if (user?.email) {
+        if (user?.shelterId) {
+          navigate("/admin-dashboard")
+        } else {
+          navigate("/")
+        }
+        return null
       }
-      return null
     }
-  
   }
 
   return (
-
     <div className="login-form">
-
-      {/* login card form (left) */}
       <div className="login-card">
-        <h2>Login</h2>
-        <form className="form">
+        <h2 className="mb-3">Login</h2>
+        <Form className="form" noValidate validated={isValidated} onSubmit={handleOnFormSubmit}>
+          {error ? <Alert className="form-item" variant='danger'><BsX height="32px" /> {error}</Alert> : null}
+          <Form.Group controlId="validationCustom01" className="form-item">
+            <FloatingLabel controlId="floatingInput" label="Email Address" className="mb-3">
+              <Form.Control
+                name="email"
+                type="email"
+                onChange={handleOnInputChange}
+                required
+                placeholder="Email Address" 
+                className="form-input" />
+            <Form.Control.Feedback type="invalid">Please enter a valid email address</Form.Control.Feedback>
+            </FloatingLabel>
+          </Form.Group>
 
-          {/* email input */}
-          <div className="input-field">
-            <label htmlFor="email">Email</label>
-            <input className="form-input"
-              name="email"
-              type="email"
-              value={form.email}
-              onChange={handleOnInputChange}
-            />
-            {errors.email && <span className="error">{errors.email}</span>}
-          </div>
-
-          {/* password input */}
-          <div className="input-field">
-            <label htmlFor="password">Password</label>
-            <input className="form-input"
-              name="password"
-              type="password"
-              value={form.password}
-              onChange={handleOnInputChange}
-            />
-            {errors.password && <span className="error">{errors.password}</span>}
-          </div>
-
-          {/* submit button */}
-          <button className="submit-login" onClick={handleOnFormSubmit}>Login</button>
-
-        </form>
+          <Form.Group controlId="validationCustom02" className="form-item">
+            <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3 form-input">
+            <Form.Control
+                name="password"
+                type="password"
+                onChange={handleOnInputChange}
+                required
+                placeholder="Password" />
+            </FloatingLabel>
+          </Form.Group>
+        
+          <Button type="submit" className="mb-2 form-item">Login</Button>
+          <Form.Text>Need an account? <a href="/register">Sign up here.</a></Form.Text>
+        </Form>
 
       </div>
-
-      {/* login image (right) */}
-      <div className="login-image">
-        <img src="" alt="Login Image" />
-      </div>
-
     </div>
   )
 }
