f462a3cebc96e36fc25e2cfed29ebaf07738096a
Annesa Tran <annesat2020@gmail.com>
2022-07-26 19:51:30 -0700

    Finished filters and added badges to accordian headers

--- balto-ui/src/components/DogSearchPage/DogSearchPage.jsx
+++ balto-ui/src/components/DogSearchPage/DogSearchPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Accordion from 'react-bootstrap/Accordion'
 import Form from 'react-bootstrap/Form'
+import Badge from 'react-bootstrap/Badge'
 import BreedSearchbar from '../BreedSearchbar/BreedSearchbar'
 import ShelterSearchbar from '../ShelterSearchbar/ShelterSearchbar'
 
@@ -9,19 +10,43 @@ export default function DogSearchPage() {
   // options that will show up on the filters, not used for anything else
   const sizeOptions = ["Small", "Medium", "Large"]
   const genderOptions = ["Male", "Female"]
-  const goodWithOptions = ["Kids", "Strangers", "Other pets"]
-  const distanceOptions = ["5 miles", "10 miles", "15 miles"]
+  const goodWithOptions = ["Kids", "Strangers", "Other dogs"]
+  const distanceOptions = [5, 10, 15]
 
-  const [selectedBreeds, setSelectedBreeds] = React.useState([])
-  const [selectedShelters,  setSelectedShelters] = React.useState([])
+  // states each of the filters
+  const [selectedBreeds, setSelectedBreeds] = React.useState([]) // ex. ["Dalmation", "Labrador"]
+  const [selectedSizes, setSelectedSizes] = React.useState([]) // ex. ["S", "M"]
+  const [selectedGenders, setSelectedGenders] = React.useState([]) // ex. ["M", "F"]
+  const [selectedGoodWith, setSelectedGoodWith] = React.useState([]) // ex. ["Kids", "Strangers"]
+  const [selectedDistance, setSelectedDistance] = React.useState('') // integer, or false if distance isn't selected
+  const [selectedShelters,  setSelectedShelters] = React.useState([]) // selected shelters by their Id, ex. [3, 4]
+
+  console.log(111, selectedDistance)
+
+  // function to handle checking/unchecking checkboxes used in the size, gender, and good with filters
+  //    takes a state var and setState var  as params (ex. selectedSizes and setSelectedSizes)
+  const handleCheck = (evt, stateArr, setStateArr) => {
+    let newArr = [...stateArr]
+    if (evt.target.checked) { // checking if the checkbox just got checked or unchecked
+      newArr = [ ...stateArr, evt.target.value ]
+    } else {
+      newArr.splice(stateArr.indexOf(evt.target.value), 1)
+    }
+    setStateArr(newArr) // updating the stateArr here
+  }
 
   return (
     <div className="filter-sidebar">
+
+      {/* alwaysOpen allows multiple filters to be open at once */}
       <Accordion alwaysOpen>
 
         {/* breed filter (dropdown search) */}
         <Accordion.Item eventKey="breed">
-          <Accordion.Header>Breed</Accordion.Header>
+          <Accordion.Header>
+            Breed
+            {selectedBreeds.length > 0 ? <Badge pill bg="primary">{selectedBreeds.length}</Badge> : null}
+          </Accordion.Header>
           <Accordion.Body>
             <BreedSearchbar selectedBreeds={selectedBreeds} setSelectedBreeds={setSelectedBreeds} />
           </Accordion.Body>
@@ -29,12 +54,19 @@ export default function DogSearchPage() {
 
         {/* size filter (checkboxes) */}
         <Accordion.Item eventKey="size">
-          <Accordion.Header>Size</Accordion.Header>
+          <Accordion.Header>
+            Size
+            {selectedSizes.length > 0 ? <Badge pill bg="primary">{selectedSizes.length}</Badge> : null}
+          </Accordion.Header>
           <Accordion.Body>
             <Form>
               {sizeOptions.map((option) => (
                 <div key={option} className="mb-3">
-                  <Form.Check type='checkbox' id={option} label={option} />
+                  <Form.Check type='checkbox'
+                    id={option}
+                    label={option}
+                    value={option}
+                    onChange={(evt)=>{handleCheck(evt, selectedSizes, setSelectedSizes)}} />
                 </div>
               ))}
             </Form>
@@ -43,12 +75,19 @@ export default function DogSearchPage() {
 
         {/* gender filter (checkboxes) */}
         <Accordion.Item eventKey="gender">
-          <Accordion.Header>Gender</Accordion.Header>
+          <Accordion.Header>
+            Gender
+            {selectedGenders.length > 0 ? <Badge pill bg="primary">{selectedGenders.length}</Badge> : null}
+          </Accordion.Header>
           <Accordion.Body>
             <Form>
               {genderOptions.map((option) => (
                 <div key={option} className="mb-3">
-                  <Form.Check type='checkbox' id={option} label={option} />
+                  <Form.Check type='checkbox'
+                    id={option}
+                    label={option}
+                    value={option}
+                    onChange={(evt)=>{handleCheck(evt, selectedGenders, setSelectedGenders)}} />
                 </div>
               ))}
             </Form>
@@ -57,12 +96,19 @@ export default function DogSearchPage() {
 
         {/* good with filter (checkboxes) */}
         <Accordion.Item eventKey="good-with">
-          <Accordion.Header>Good With</Accordion.Header>
+          <Accordion.Header>
+            Good With
+            {selectedGoodWith.length > 0 ? <Badge pill bg="primary">{selectedGoodWith.length}</Badge> : null}
+          </Accordion.Header>
           <Accordion.Body>
             <Form>
               {goodWithOptions.map((option) => (
                 <div key={option} className="mb-3">
-                  <Form.Check type='checkbox' id={option} label={option} />
+                  <Form.Check type='checkbox'
+                  id={option}
+                  label={option}
+                  value={option}
+                  onChange={(evt)=>{handleCheck(evt, selectedGoodWith, setSelectedGoodWith)}} />
                 </div>
               ))}
             </Form>
@@ -71,14 +117,22 @@ export default function DogSearchPage() {
 
         {/* distance filter (radio buttons) */}
         <Accordion.Item eventKey="distance">
-          <Accordion.Header>Distance</Accordion.Header>
+          <Accordion.Header>
+            Distance
+            {selectedDistance && <Badge pill bg="primary">1</Badge>}
+          </Accordion.Header>
           <Accordion.Body>
             <Form>
-            <Form.Select aria-label="distance-select">
-              <option value={null}>Anywhere</option>
-              <option value={5}>Within 5 miles</option>
-              <option value={10}>Within 10 miles</option>
-              <option value={3}>Within 15 miles</option>
+            <Form.Select
+              aria-label="distance-select"
+              // onChange prop updates distance choice
+              onChange={(evt) => { setSelectedDistance(evt.target.value) }} >
+                {/* default option to not specify distance constraint */}
+                <option value={''}>Anywhere</option>
+                {/* rest of the distance options */}
+                {distanceOptions.map((option) => (
+                    <option key={option} value={option}>Within {option} miles</option>
+                ))}
             </Form.Select>
             </Form>
           </Accordion.Body>
@@ -86,12 +140,14 @@ export default function DogSearchPage() {
 
         {/* shelter filter (dropdown search) */}
         <Accordion.Item eventKey="shelter">
-          <Accordion.Header>Shelter</Accordion.Header>
+          <Accordion.Header>
+            Shelter
+            {selectedShelters.length > 0 ? <Badge pill bg="primary">{selectedShelters.length}</Badge> : null}
+            </Accordion.Header>
           <Accordion.Body>
             <ShelterSearchbar selectedShelters={selectedShelters} setSelectedShelters={setSelectedShelters} />
           </Accordion.Body>
         </Accordion.Item>
-
 
       </Accordion>
     </div>
