dbfd85d3396cb6f0d41f44117e2b843517c12d80
Annesa Tran <annesat2020@gmail.com>
2022-07-28 09:40:18 -0700

    Fixed naming error for Dogs model

--- balto-api/routes/dogs.js
+++ balto-api/routes/dogs.js
@@ -1,14 +1,17 @@
 const express = require("express")
 const router = express.Router()
-const Dog = require("../models/dogs")
+const Dogs = require("../models/dogs")
 
 // get the dog for the given dog id
 router.get("/:dogId", async (req, res, next) => {
     try {
         const dogId = req.params.dogId
-        const dog = await Dog.fetchDogInfo(dogId)
+        const dog = await Dogs.fetchDogInfo(dogId)
         return res.status(200).json({ dog })
     } catch (err) {
+        next(err)
+    }
+})
 
 // view dog search results
 router.post("/", async (req, res, next) => {
