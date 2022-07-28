ae12158510f1a12b7c97190c7e977c4e5bbd21db
charlesxu27 <charlesxu27@gmail.com>
2022-07-28 08:37:49 -0700

    completed starred page (data called from backend) and connected backend to dog profile page

--- balto-ui/src/components/DogProfile/DogProfile.jsx
+++ balto-ui/src/components/DogProfile/DogProfile.jsx
@@ -10,9 +10,9 @@ import ApiClient from '../../services/ApiClient';
 
 export default function DogProfile() {
 
-  const [isLoading, setIsLoading] = React.useState(false)
-  const [error, setError] = React.useState(null)
-  const [dogInfo, setDogInfo] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [dogInfo, setDogInfo] = useState({})
   const { dogId } = useParams()
 
   useEffect(() => {
@@ -21,7 +21,6 @@ export default function DogProfile() {
       const { data, error } = await ApiClient.fetchDogById(id)
       if (data?.dog) {
         setDogInfo(data.dog[0])
-        // console.log(100, dogInfo.dog)
       }
       if (error) {
         setError(error)
@@ -31,68 +30,71 @@ export default function DogProfile() {
     }
     getDog(dogId)
     // TODO: can't use await keyword above...so need to deconstruct object on call (setDogInfo(data.dog[0])) or else risks synchronous array destructuring to throw key not found error
+    // SOLUTION: set default state variable dogInfo to empty object
   }, []);
+  { console.log("dogInfo object after useEffect: ", dogInfo) }
 
-  console.log("dogInfo object after useEffect: ", dogInfo)
 
-  return (
-    <div className='profile-page'>
-      <div className='banner'>
-        <img src="#" className='banner-img'></img>
-      </div>
-      <div className='split-pane'>
-        <div className='profile-pane'>
-          <div className='profile-header'>
-            <img src="#" className='profile-img'></img>
-            <div className='profile-title'>
-              <span>Hi, I'm <h1>{dogInfo.name}</h1></span>
-              <section className='tags'>
-                {/* <span className='tag'>{dog.breed}</span>
-                <span className='tag'>{dog.dob}</span>
-                <span className='tag'>{dog.size}</span>
-                <span className='tag'>{dog.sex === 'm' ? 'male' : 'female'}</span> */}
+  // if (dogInfo) { // TODO: hacky solution to prevent object undefined errors
+    return (
+      <div className='profile-page'>
+        <div className='banner'>
+          <img src="#" className='banner-img'></img>
+        </div>
+        <div className='split-pane'>
+          <div className='profile-pane'>
+            <div className='profile-header'>
+              <img src="#" className='profile-img'></img>
+              <div className='profile-title'>
+                <span>Hi, I'm <h1>{dogInfo.name}</h1></span>
+                <section className='tags'>
+                  <span className='tag'>{dogInfo.breed}</span>
+                <span className='tag'>{dogInfo.dob}</span>
+                <span className='tag'>{dogInfo.size}</span>
+                <span className='tag'>{dogInfo.sex === 'm' ? 'male' : 'female'}</span>
+                </section>
+              </div>
+            </div>
+            <div className='about-section'>
+              <section className='desc-1'>
+                <h2>I'm known for being...</h2>
+                {/* <p>{dog.desc_1}</p> */}
+              </section>
+              <section className='desc-2'>
+                <h2>I'm looking for someone who...</h2>
+                {/* <p>{dog.desc_2}</p> */}
+              </section>
+              <section className='shelter-loc'>
+                <h2>You can meet me at...</h2>
+                <p>Wags for Treats Animal Shelter</p>
+                <p>2700 Ninth St, Berkeley, CA, 94710</p>
               </section>
             </div>
           </div>
-          <div className='about-section'>
-            <section className='desc-1'>
-              <h2>I'm known for being...</h2>
-              {/* <p>{dog.desc_1}</p> */}
+          <div className='training-pane'>
+            <section className='action-btns'>
+              <div className='btn'>
+                <Link to="/star">
+                  <BsStar />Favorite</Link>
+              </div>
+              <div className='btn'>
+                <Link className='btn-txt' to="/adopt">
+                  <BsFillHouseDoorFill /> Adopt Me</Link>
+              </div>
             </section>
-            <section className='desc-2'>
-              <h2>I'm looking for someone who...</h2>
-              {/* <p>{dog.desc_2}</p> */}
+            <section className='progress-bar'>
+              <b>Progress in the shelter's training milestones</b>
+              <div></div>
             </section>
-            <section className='shelter-loc'>
-              <h2>You can meet me at...</h2>
-              <p>Wags for Treats Animal Shelter</p>
-              <p>2700 Ninth St, Berkeley, CA, 94710</p>
-            </section>
-          </div>
-        </div>
-        <div className='training-pane'>
-          <section className='action-btns'>
-            <div className='btn'>
-              <Link to="/star">
-                <BsStar />Favorite</Link>
-            </div>
-            <div className='btn'>
-              <Link className='btn-txt' to="/adopt">
-                <BsFillHouseDoorFill /> Adopt Me</Link>
-            </div>
-          </section>
-          <section className='progress-bar'>
-            <b>Progress in the shelter's training milestones</b>
-            <div></div>
-          </section>
-          <div className='training-feed'>
-            <h2>Training Feed</h2>
-            <div className='feed'>
-              <TrainingFeed milestones={milestones} />
+            <div className='training-feed'>
+              <h2>Training Feed</h2>
+              <div className='feed'>
+                <TrainingFeed milestones={milestones} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  // }
 }
