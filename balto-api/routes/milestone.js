9d1f3092fa801590f2937927f9fbe5a4bac5ae57
charlesxu27 <charlesxu27@gmail.com>
2022-08-01 00:17:08 -0700

    added routes and model for milestone entity

--- balto-api/routes/milestone.js
+++ balto-api/routes/milestone.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const Milestone = require("../models/milestone")
+const security = require("../middleware/security")
 
 
-// get the dog for the given dog id
+
+// get all the milestones for a dog
 router.get("/view/:dogId", async (req, res, next) => {
     try {
         const dogId = req.params.dogId
@@ -14,11 +16,42 @@ router.get("/view/:dogId", async (req, res, next) => {
     }
 })
 
-// router.post("/", async (req, res, next) => {
+// TODO: add in security middleware!!!
 
-// })
+// create a milestone for a given dog
+router.post("/", async (req, res, next) => {
+    try {
+        const { dogId } = req.body
+        const newMilestoneForm  = req?.body
+        const milestone = await Milestone.createMilestone({ newMilestoneForm, dogId} )
+        return res.status(201).json( { milestone: milestone } )
+    } catch(err) {
+        next(err)
+    }
+})
 
+// update a milestone for a given dog
+router.put("/:milestoneId", async(req, res, next) => {
+    try {
+        const milestoneId = req.params.dogId
+        const updatedMilestoneForm = req?.body
+        const updatedMilestone = await Milestone.updateMilestone(updatedMilestoneForm, milestoneId)
+        return res.status(200).json( { milestone: updatedMilestone } )
+    } catch (err) {
+        next(err)
+    }
+})
 
+// delete a milestone for a given dog
+router.delete("/:milestoneId", async(req, res, next) => {
+    try {
+        const milestoneId = req.params.dogId
+        const deletedMilestone = await Milestone.deleteMilestone(milestoneId)
+        return res.status(200).json( { milestone: deletedMilestone } )
+    } catch (err) {
+        next(err)
+    }
+})
 
 
 module.exports = router
