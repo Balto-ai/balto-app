7ebbbb25f3799ad70325ccbf8f97f48aabafbae4
Annesa Tran <annesat2020@gmail.com>
2022-08-02 14:45:58 -0700

    Added context menu to data grid

--- balto-ui/src/components/ShelterOverview/ShelterOverview.jsx
+++ balto-ui/src/components/ShelterOverview/ShelterOverview.jsx
@@ -6,9 +6,11 @@ import { DataGrid, GridToolbarContainer, GridToolbarColumnsButton, GridToolbarFi
 import { styled } from '@mui/material/styles'
 import Pagination from '@mui/material/Pagination'
 import PaginationItem from '@mui/material/PaginationItem'
-import Dropdown from 'react-bootstrap/Dropdown'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import { FiMoreHorizontal } from 'react-icons/fi'
 import './ShelterOverview.css'
 
 // used to add styling
@@ -47,7 +49,7 @@ const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
 export default function ShelterOverview() {
 
     const { dogRecords, error, isLoading, getAgeGroup } = useDogRecordsContext()
-
+    const navigate = useNavigate()
     const [searchQuery, setSearchQuery] = React.useState("")
 
     // filter dogs based on search query
@@ -61,8 +63,8 @@ export default function ShelterOverview() {
         { field: 'image_url', headerName: 'Image', sortable: false, filterable: false, width: 70,
             renderCell: (params) => <img src={params.value} className="dog-record-image" /> },
         { field: 'name', headerName: 'Name', width: 150,
-            renderCell: (params) => <div className="capitalized">{params.value}</div> },
-        { field: 'breed', headerName: 'Breed', width: 150 },
+            renderCell: (params) => <div className="capitalized dog-name" onClick={()=>{navigate(`/admin-dashboard/dog-record/id/${params.id}`)}}>{params.value}</div> },
+        { field: 'breed', headerName: 'Breed', width: 130 },
         { field: 'sex', headerName: 'Sex', width: 90,
             renderCell: (params) => <div>{params.value === "m" ? "Male" : "Female"}</div> },
         { field: 'size', headerName: 'Size', width: 90,
@@ -74,20 +76,85 @@ export default function ShelterOverview() {
         { field: 'date_entered', headerName: 'Date Entered', width: 150,
             renderCell: (params) => <div>{(new Date(params.value)).toLocaleDateString()}</div> },
         { field: 'updated_at', headerName: 'Last Updated', width: 150,
-            renderCell: (params) => <div>{(new Date(params.value)).toLocaleDateString()}</div> }
+            renderCell: (params) => <div>{(new Date(params.value)).toLocaleDateString()}</div> },
       ]
 
     return (
-        <div className="shelter-overview primary-container" style={{ display: 'flex', height: 700, width: '100%' }}>
-            <DogRecordSearch setSearchQuery={setSearchQuery} />
-            <ShelterOverviewGrid rows={rows} columns={columns} />
+        <div className="shelter-overview primary-container" style={{ display: 'flex', height: '100%', width: '100%' }}>
+            <div className="top-bar">
+                <DogRecordSearch setSearchQuery={setSearchQuery} />
+                <Button href="/admin-dashboard/add-record" className="dog-record-add-btn mb-3">+ New</Button>
+            </div>
+            {/* <ShelterOverviewGrid rows={rows} columns={columns} /> */}
+            <RowContextMenu rows={rows} columns={columns} />
         </div>
     )
 }
 
+export function RowContextMenu(props) {
+    const [selectedRow, setSelectedRow] = React.useState()
+    const navigate = useNavigate()
+  
+    const [contextMenu, setContextMenu] = React.useState(null)
+  
+    const handleContextMenu = (event) => {
+      event.preventDefault();
+      setSelectedRow(Number(event.currentTarget.getAttribute('data-id')));
+      setContextMenu(
+        contextMenu === null
+          ? { mouseX: event.clientX - 2, mouseY: event.clientY - 4 }
+          : null,
+      )
+    }
+  
+    const handleClose = () => {
+      setContextMenu(null);
+    }
+  
+    return (
+      <div style={{ width: '100%' }}>
+        <ShelterOverviewGrid
+            rows={props.rows}
+            columns={props.columns}
+            componentsProps={{
+                row: {
+                    onContextMenu: handleContextMenu,
+                    style: { cursor: 'context-menu' },
+                }
+            }}
+        />
+
+        <Menu
+          open={contextMenu !== null}
+          onClose={handleClose}
+          anchorReference="anchorPosition"
+          anchorPosition={
+            contextMenu !== null
+              ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+              : undefined
+          }
+          componentsProps={{
+            root: {
+              onContextMenu: (e) => {
+                e.preventDefault()
+                handleClose()
+              },
+            },
+          }}
+        >
+          {/* might want to just replace with <a href> links */}
+          <MenuItem onClick={()=>{navigate("/admin-dashboard/dog-record/id/"+selectedRow)}} disableRipple className="context-menu-item">Go to details</MenuItem>
+          <MenuItem onClick={()=>{navigate("/admin-dashboard/dog-record/id/"+selectedRow+"/edit")}} disableRipple className="context-menu-item">Edit record</MenuItem>
+          {/* delete button here is mockup; should be the delete modal used in the dog record detail page */}
+          <MenuItem onClick={()=>{}} disableRipple className="context-menu-item" sx={{ color: "red"}}>Delete</MenuItem>
+        </Menu>
+      </div>
+    )
+  }
+
 export function ShelterOverviewGrid(props) {
 return (
-    <div style={{ flexGrow: 1, height: 700}} className="shelter-overview-grid">
+    <div style={{ flexGrow: 1}} className="shelter-overview-grid">
         <StyledDataGrid
             rows={props.rows}
             columns={props.columns}
@@ -99,6 +166,7 @@ return (
                 Toolbar: CustomToolbar,
                 Pagination: CustomPagination
             }}
+            componentsProps={props.componentsProps}
         />
     </div>
     )
