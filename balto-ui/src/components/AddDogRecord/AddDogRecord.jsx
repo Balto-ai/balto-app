79a2bc7c57aa29c69c4ef4a95a4e5798c19fc5cf
chcx97 <xiongchristy@gmail.com>
2022-08-03 14:55:46 -0700

    finished image upload

--- balto-ui/src/components/AddDogRecord/AddDogRecord.jsx
+++ balto-ui/src/components/AddDogRecord/AddDogRecord.jsx
@@ -20,9 +20,6 @@ import Toast from 'react-bootstrap/Toast';
 import DogIcon from './icon/paw (1).png'
 import Box from '@mui/material/Box';
 import ImagePlaceholder from './icon/image (1).png'
-import PropTypes from 'prop-types';
-import CircularProgress from '@mui/material/CircularProgress';
-import Typography from '@mui/material/Typography';
 
 
 
@@ -407,50 +404,3 @@ export function ShowToast({setShow, show}) {
     </div>
   );
 }
-// function CircularProgressWithLabel(props) {
-//   return (
-//     <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-//       <CircularProgress color='purple' variant="determinate" {...props} />
-//       <Box
-//         sx={{
-//           top: 0,
-//           left: 0,
-//           bottom: 0,
-//           right: 0,
-//           position: 'absolute',
-//           display: 'flex',
-//           alignItems: 'center',
-//           justifyContent: 'center',
-//         }}
-//       >
-//         <Typography variant="caption" component="div" color="text.secondary">
-//           {`${Math.round(props.value)}%`}
-//         </Typography>
-//       </Box>
-//     </Box>
-//   );
-// }
-
-// CircularProgressWithLabel.propTypes = {
-//   /**
-//    * The value of the progress indicator for the determinate variant.
-//    * Value between 0 and 100.
-//    * @default 0
-//    */
-//   value: PropTypes.number.isRequired,
-// };
-
-// export function CircularStatic() {
-//   const [progress, setProgress] = React.useState(10);
-
-//   React.useEffect(() => {
-//     const timer = setInterval(() => {
-//       setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
-//     }, 2000);
-//     return () => {
-//       clearInterval(timer);
-//     };
-//   }, []);
-
-//   return <CircularProgressWithLabel value={progress} />;
-// }
