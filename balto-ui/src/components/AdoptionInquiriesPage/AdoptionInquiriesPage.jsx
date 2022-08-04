6b9df3a6d2e0df5bde23c5ec65ccba379352e42c
Annesa Tran <annesat2020@gmail.com>
2022-08-03 19:41:28 -0700

    Created data grid columns

--- balto-ui/src/components/AdoptionInquiriesPage/AdoptionInquiriesPage.jsx
+++ balto-ui/src/components/AdoptionInquiriesPage/AdoptionInquiriesPage.jsx
@@ -20,22 +20,33 @@ export function AdoptionInquiriesPage() {
 
     // filter dogs based on search query
     let adoptionInquiriesToRender = [...allAdoptionInquiries]
-    console.log(searchQuery)
+
     if (searchQuery !== "") {
-      adoptionInquiriesToRender = adoptionInquiriesToRender.filter(adoptionInquiry => adoptionInquiry.email.toLowerCase().includes(searchQuery.toLowerCase()))
+      adoptionInquiriesToRender = adoptionInquiriesToRender.filter(adoptionInquiry => adoptionInquiry.dog_name.toLowerCase().includes(searchQuery.toLowerCase()))
     }
 
     const rows = [...allAdoptionInquiries]
     const columns = [
-        { field: 'email', headerName: 'Email', width: 150 },
-        { field: 'phone_number', headerName: 'Phone Number', width: 150 },
-        { field: 'created_at', headerName: 'Timestamp', width: 150 }
-      ]
+      { field: 'created_at', headerName: 'Timestamp', width: 160, type: 'dateTime',
+        valueFormatter: (params) => (new Date(params.value)).toLocaleString()},
+      { field: 'dog_name', headerName: 'Dog', width: 160,
+        renderCell: (params) =>
+          <div className="capitalized">
+            <img className="dog-image-icon" src={params.row.dog_image_url} alt={`"Image of ${params.value}`} />
+            {params.value}
+          </div> },
+      { field: 'user_first_name', headerName: 'First Name', width: 120,
+        renderCell: (params) => <div className="capitalized">{params.value}</div> },
+      { field: 'email', headerName: 'Email', width: 140 },
+      { field: 'phone_number', headerName: 'Phone Number', width: 130, sortable: false, filterable: false },
+      { field: 'zipcode', headerName: 'Zip Code', width: 100 },
+      { field: 'comments', headerName: 'Comments', flex: 1, sortable: false, filterable: false }
+    ]
 
     return (
         <div className="adoption-inquiries-page primary-container" style={{ display: 'flex', height: '100%', width: '100%' }}>
             <div className="top-bar">
-                <Searchbar setSearchQuery={setSearchQuery} />
+                <Searchbar setSearchQuery={setSearchQuery} placeholder={"Search by dog name"} />
             </div>
             <CustomDataGrid rows={rows} columns={columns} />
         </div>
