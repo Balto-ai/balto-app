2d7536be801d80d02e67967da5ede5b3acca86f5
chcx97 <xiongchristy@gmail.com>
2022-08-08 17:23:22 -0700

    login to access and not found page done

--- balto-ui/src/components/LoginToAccess/LoginToAccess.jsx
+++ balto-ui/src/components/LoginToAccess/LoginToAccess.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { Box } from '@mui/material';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import ErrorDog from './image/Error Naughty Dog.svg'
+
 
 export default function LoginToAccess() {
   return (
-    <div>LoginToAccess</div>
+    <Box  px={{xs: 3, sm: 10}} color='black' py={{xs: 8, sm:12}}>
+      <Container maxWidth='sm'>
+        <Grid justifyContent='center' alignItems='center' textAlign='center'>
+          <Grid item>
+          
+            <Box>
+              <h4 className='nf-title'>Access denied</h4>
+              <span>
+              <Link  href="/login" underline="hover">Login</Link> or <Link  href="/register" underline="hover">sign up</Link> to gain access to this page
+              </span>
+            
+            <img width={300} height={300} className='doggie-nf' src={ErrorDog} alt="Not Found Page"></img>
+            </Box>
+          
+          </Grid>
+          <Grid item>
+            <Box>
+            
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   )
 }
