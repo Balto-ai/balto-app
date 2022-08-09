82c2058c48c2f8d9f9a5e6a94faa017ff535f84d
chcx97 <xiongchristy@gmail.com>
2022-08-08 18:02:10 -0700

    modified access forbidden and access denied

--- balto-ui/src/components/AccessForbidden/AccessForbidden.jsx
+++ balto-ui/src/components/AccessForbidden/AccessForbidden.jsx
@@ -12,19 +12,16 @@ export default function AccessForbidden() {
       <Grid justifyContent='center' alignItems='center' textAlign='center'>
         <Grid item>
         
-          <Box sx={{marginBottom:10}}>
+          <Box>
             <h4 className='nf-title'>Access forbidden</h4>
             <span>
             <Link  href="/login" underline="hover">Login</Link> to gain access to this page
             </span>
-          
-          <img width={300} height={300} className='doggie-nf' src={ErrorDog} alt="Not Found Page"></img>
           </Box>
-        
         </Grid>
         <Grid item>
-          <Box>
-          
+          <Box sx={{marginBottom:10}}>
+          <img width={300} height={300} className='doggie-nf' src={ErrorDog} alt="Not Found Page"></img>
           </Box>
         </Grid>
       </Grid>
--- balto-ui/src/components/LoginToAccess/LoginToAccess.jsx
+++ balto-ui/src/components/LoginToAccess/LoginToAccess.jsx
@@ -13,19 +13,19 @@ export default function LoginToAccess() {
         <Grid justifyContent='center' alignItems='center' textAlign='center'>
           <Grid item>
           
-            <Box sx={{marginBottom:10}}>
+            <Box >
               <h4 className='nf-title'>Access denied</h4>
               <span>
               <Link  href="/login" underline="hover">Login</Link> or <Link  href="/register" underline="hover">sign up</Link> to gain access to this page
               </span>
             
-            <img width={300} height={300} className='doggie-nf' src={ErrorDog} alt="Not Found Page"></img>
+           
             </Box>
           
           </Grid>
           <Grid item>
-            <Box>
-            
+            <Box sx={{marginBottom:10}}>
+            <img width={300} height={300} className='doggie-nf' src={ErrorDog} alt="Not Found Page"></img>
             </Box>
           </Grid>
         </Grid>
