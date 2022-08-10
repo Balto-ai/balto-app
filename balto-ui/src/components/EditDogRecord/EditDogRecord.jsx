381f5604813097fa8bc6d808f1bbcc86b8d8211e
Annesa Tran <annesat2020@gmail.com>
2022-08-09 18:53:09 -0700

    Changed all paw icons to bone icons for ratings

--- balto-ui/src/components/EditDogRecord/EditDogRecord.jsx
+++ balto-ui/src/components/EditDogRecord/EditDogRecord.jsx
@@ -9,7 +9,8 @@ import Alert from 'react-bootstrap/Alert'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Rating from '@mui/material/Rating'
-import { IoPaw, IoPawOutline } from 'react-icons/io5'
+import EmptyBone from "../Icon/EmptyBone"
+import FilledBone from "../Icon/FilledBone"
 import { BsX } from "react-icons/bs"
 import "./EditDogRecord.css"
 import Toast from 'react-bootstrap/Toast';
@@ -284,8 +285,8 @@ export default function EditDogRecord() {
                     name={category}
                     value={form[category] || dogRecord[category]}
                     onChange={handleOnRatingChange}
-                    icon={<IoPaw fontSize="inherit" />}
-                    emptyIcon={<IoPawOutline fontSize="inherit" />}
+                    icon={<FilledBone fontSize="inherit" />}
+                    emptyIcon={<EmptyBone fontSize="inherit" />}
                     getLabelText={(value) => `Rating ${value}`}
                     required />
                 </Form.Group>
