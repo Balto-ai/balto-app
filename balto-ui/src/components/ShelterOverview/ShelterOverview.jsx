966a43f0456720d0ecd5e793ec5fe8f50a682173
chcx97 <xiongchristy@gmail.com>
2022-08-10 11:16:32 -0700

    delete works on shelter overview

--- balto-ui/src/components/ShelterOverview/ShelterOverview.jsx
+++ balto-ui/src/components/ShelterOverview/ShelterOverview.jsx
@@ -9,20 +9,24 @@ import { Searchbar } from '../CustomDataGrid/CustomDataGrid'
 import CustomDataGrid from '../CustomDataGrid/CustomDataGrid'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './ShelterOverview.css'
-import AddIcon from '@mui/icons-material/Add'; 
+import { storage } from '../../firebase/firebase'
+import { ref, deleteObject } from 'firebase/storage'
+import Modal from 'react-bootstrap/Modal'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function ShelterOverview() {
 
     const { dogRecords, error, isLoading, getAgeGroup } = useDogRecordsContext()
     const navigate = useNavigate()
     const [searchQuery, setSearchQuery] = React.useState("")
-
+    const [modalShow, setModalShow] = React.useState(false)
     // filter dogs based on search query
     let dogRecordsToRender = [...dogRecords]
     if (searchQuery !== "") {
         dogRecordsToRender = dogRecordsToRender.filter(dogRecord => dogRecord.name.toLowerCase().includes(searchQuery.toLowerCase()))
     }
-
+    
     const rows = [...dogRecordsToRender]
     const columns = [
         { field: 'name', headerName: 'Name', width: 170,
@@ -49,11 +53,12 @@ export default function ShelterOverview() {
 
     return (
         <div className="shelter-overview primary-container" style={{ display: 'flex', height: '100%', width: '100%' }}>
+            <h1 className='dogs-overview-title'>Shelter Overview</h1>
             <div className="top-bar">
                 <Searchbar setSearchQuery={setSearchQuery} placeholder={"Search by dog name"}/>
                 <Button style={{color:'white'}} href="/admin-dashboard/add-record" className="dog-record-add-btn mb-3">+ New</Button>
             </div>
-            <RowContextMenu rows={rows} columns={columns} />
+            <RowContextMenu show={modalShow} setModalShow={setModalShow} rows={rows} columns={columns} />
         </div>
     )
 }
@@ -62,9 +67,8 @@ export default function ShelterOverview() {
 export function RowContextMenu(props) {
     const [selectedRow, setSelectedRow] = React.useState()
     const navigate = useNavigate()
-  
     const [contextMenu, setContextMenu] = React.useState(null)
-  
+    const {setModalShow, show}= props
     const handleContextMenu = (event) => {
       event.preventDefault();
       setSelectedRow(Number(event.currentTarget.getAttribute('data-id')));
@@ -114,9 +118,56 @@ export function RowContextMenu(props) {
           <MenuItem onClick={()=>{navigate("/admin-dashboard/dog-record/id/"+selectedRow)}} disableRipple className="context-menu-item">Go to details</MenuItem>
           <MenuItem onClick={()=>{navigate("/admin-dashboard/dog-record/id/"+selectedRow+"/edit")}} disableRipple className="context-menu-item">Edit record</MenuItem>
           {/* delete button here is mockup; should be the delete modal used in the dog record detail page */}
-          <MenuItem onClick={()=>{}} disableRipple className="context-menu-item" sx={{ color: "red"}}>Delete</MenuItem>
+          <MenuItem onClick={()=>{ setModalShow(true); handleClose();}} disableRipple className="context-menu-item" sx={{ color: "red"}}>Delete</MenuItem>
         </Menu>
+        {show &&  <DeleteDogRecordModal rows={props.rows} dogId={selectedRow} show={show} onHide={() => setModalShow(false)} />}
       </div>
     )
   }
+  export function DeleteDogRecordModal(props) {
 
+    const { deleteDogRecord } = useDogRecordsContext()
+    const {rows, dogId, show, onHide} = props;
+    const [imageName, setImageName] = useState('')
+    const [selectedDog, setSelectedDog] = useState({})
+    const navigate = useNavigate()
+    useEffect(()=>{
+      if (rows.length === 0)  return;
+      else{
+      const dog = rows.filter((row)=>{
+        return row.id === dogId;
+      })
+      setImageName(dog[0].image_name)
+      }
+    },[])
+    const handleOnDelete = async () => {
+      const oldImageRef = ref(storage, `dogProfileImages/${imageName}`);
+      // Delete the file
+      deleteObject(oldImageRef).then(() => {
+        // File deleted successfully
+        // setImageDeleted(true)
+      }).catch((error) => {
+        // Uh-oh, an error occurred!
+        console.error(error)
+      });
+      await deleteDogRecord(props.dogId)
+      navigate('/admin-dashboard')
+      onHide()
+    }
+    return (
+      <Modal
+        {...props}
+        size="m"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton></Modal.Header>
+        {/* modal body is the form to login the user */}
+        <Modal.Body>
+            <p>Are you sure you want to delete this dog record? This cannot be undone.</p>
+            <Button variant="outline-danger" onClick={handleOnDelete}>Yes, Delete</Button>
+            <Button variant="outline-light" onClick={onHide}>Cancel</Button>
+        </Modal.Body>
+      </Modal>
+    )
+  }
