bf7157d7c67a45af8617b8b6c793a5edf05d9752
charlesxu27 <charlesxu27@gmail.com>
2022-08-10 03:21:22 -0700

    set fixed image size with auto aspect ratio adjustment

--- balto-ui/src/components/DogProfile/DogProfile.jsx
+++ balto-ui/src/components/DogProfile/DogProfile.jsx
@@ -9,9 +9,9 @@ import Modal from 'react-bootstrap/Modal'
 import { Container, Row, Col, Button, Carousel, Stack } from 'react-bootstrap'
 import { Box, Tab, Tabs, Chip } from '@mui/material'
 
-import {TabContext, TabList, TabPanel} from '@mui/lab'
+import { TabContext, TabList, TabPanel } from '@mui/lab'
 import { useAuthContext } from '../../contexts/auth';
-import {PublicImagesContextProvider, usePublicImagesContext} from '../../contexts/public-dog-images'
+import { PublicImagesContextProvider, usePublicImagesContext } from '../../contexts/public-dog-images'
 import { DogProfileContextProvider, useDogProfileContext } from '../../contexts/dog-profile';
 import ApiClient from '../../services/ApiClient';
 import { Rating, Typography } from '@mui/material'
@@ -48,21 +48,21 @@ export function DogProfile() {
   const [playfulness, setPlayfulness] = useState(0)
   const [modalShow, setModalShow] = useState(false)
   const [receiptShow, setReceiptShow] = useState(false)
-  const {images, setDogId} = usePublicImagesContext()
+  const { images, setDogId } = usePublicImagesContext()
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [userLoggedIn, setUserLoggedIn] = useState(false)
-   //tab variabls and methods
-   const [value, setValue] = React.useState('1');
-   const handleChange = (event, newValue) => {
-     setValue(newValue);
-   };
-   const handleOnClick = () => {
-    if (user?.email){
+  //tab variabls and methods
+  const [value, setValue] = React.useState('1');
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const handleOnClick = () => {
+    if (user?.email) {
       setModalShow(true)
-    }else{
+    } else {
       setShowLoginModal(true)
     }
-   }
+  }
   // set the dog attributes to state variables for ratings
   useEffect(() => {
     setKidFriendly(dogInfo.kid_friendly)
@@ -80,15 +80,15 @@ export function DogProfile() {
 
     <Container className='dog-profile primary-container'>
       <Row>
-        <Col>
+        <Col class='col-md-6 '>
           <Row className='dog-profile-image'>
-            <Carousel>
+            <Carousel class='carousel-inner'>
               <Carousel.Item>
                 <img src={dogInfo.dog_image_url} className="main-image" alt='main dog images' />
               </Carousel.Item>
-              {images.length > 0 && 
-                (images.map((image, index)=>{
-                  return(
+              {images.length > 0 &&
+                (images.map((image, index) => {
+                  return (
                     <Carousel.Item key={index}>
                       <img src={image.image_url} className="main-image" alt='more dog images' />
                     </Carousel.Item>
@@ -97,13 +97,12 @@ export function DogProfile() {
               }
             </Carousel>
           </Row>
-         
         </Col>
-        <Col className='dog-profile-details'>
+        <Col className='dog-profile-details' class='col-md-6'>
           <Row>
             <h1 className="dog-name">{dogInfo.dog_name}</h1>
           </Row>
-          <Row style={{paddingBottom: 0}} >
+          <Row style={{ paddingBottom: 0 }} >
             <Container className="applied-filters">
               <Chip className="dog-profile-chip" label={dogInfo?.breed} />
               <Chip className="dog-profile-chip" label={getAgeGroup(dogInfo?.dob)} />
@@ -111,26 +110,29 @@ export function DogProfile() {
               <Chip className="dog-profile-chip" label={dogInfo?.sex === 'm' ? 'Male' : 'Female'} />
             </Container>
           </Row>
-          <Row style={{paddingTop:15, paddingBottom: 15}}>
+          <Row style={{ paddingTop: 15, paddingBottom: 15 }}>
             <span><p>{dogInfo.desc_1 !== "" ? `I'm known for being ${dogInfo.desc_1}.` : ""}</p></span>
             <span><p>{dogInfo.desc_2 !== "" ? `I'm looking for someone who ${dogInfo.desc_2}.` : ""}</p></span>
           </Row>
           <Row>
             <section className='action-btns'>
               <Button onClick={handleOnClick} variant="secondary" className='shadow-btn' style={{ textDecoration: 'none', color: 'black', fontWeight: 'bold' }}>
-                 ADOPT ME
+                ADOPT ME
               </Button>
+
+              {/* Logic to handle adoption button click and inquiry submission */}
               <AdoptionModal setSubmittedForm={setSubmittedForm} show={modalShow} onHide={() => { setModalShow(false); setReceiptShow(true); }} dogId={dogInfo.dog_id} />
-              {showLoginModal && <LoginModal userLoggedIn={userLoggedIn} showLoginModal={showLoginModal} setShowLoginModal={setShowLoginModal} setUserLoggedIn={setUserLoggedIn} show={showLoginModal} onHide={() => {setShowLoginModal(false); setUserLoggedIn(true)}} />}
+              {showLoginModal && <LoginModal userLoggedIn={userLoggedIn} showLoginModal={showLoginModal} setShowLoginModal={setShowLoginModal} setUserLoggedIn={setUserLoggedIn} show={showLoginModal} onHide={() => { setShowLoginModal(false); setUserLoggedIn(true) }} />}
               { /* Modals for adoption inquiry user story */}
               {submittedForm && <AdoptionReceiptModal show={receiptShow} onHide={() => { setReceiptShow(false) }} dogName={dogInfo.dog_name} shelterName={dogInfo.shelterName} />}
+
               <StarButtonRect dogId={dogInfo.dog_id} dogName={dogInfo.dog_name} />
-          </section>
-      </Row>
-      
-      <Row style={{paddingTop: 35}} className='dog-profile-tabs'>
-      <Box sx={{ width: '100%' }}>
-                <TabContext value={value}>
+            </section>
+          </Row>
+
+          <Row style={{ paddingTop: 20 }} className='dog-profile-tabs'>
+            <Box sx={{ width: '100%', height: 'auto' }}>
+              <TabContext value={value}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                   <Tabs
                     value={value}
@@ -145,99 +147,82 @@ export function DogProfile() {
                   </Tabs>
                 </Box>
                 <TabPanel value='1'>
-                <Box>
-                <Row className='attributes'>
-            {/* <h2 className='photo-title'>A little more about me...</h2> */}
-            {/* <div className='attributes-list'> */}
-            <Col>
-            <Stack>
-                <Stack direction="horizontal">
-                  {noviceFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
-                  {noviceFriendly && <Typography component="legend" noWrap={true}>&nbsp; Novice Friendly</Typography>}               
-                </Stack>
-                
-                <Stack direction='horizontal'>
-                  {kidFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
-                  {kidFriendly && <Typography component="legend" noWrap={true}>&nbsp; Kid Friendly</Typography>}
-                </Stack>
-                
-                <Stack direction='horizontal'>
-                  {dogFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
-                  {dogFriendly && <Typography component="legend" noWrap={true}>&nbsp; Dog Friendly</Typography>}
-                </Stack>
-                
-                <Stack direction='horizontal'>
-                  {catFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
-                  {catFriendly && <Typography component="legend" noWrap={true}>&nbsp; Cat Friendly</Typography>}
-                </Stack>
-                
-                <Stack direction='horizontal'>
-                  {strangerFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
-                  {strangerFriendly && <Typography component="legend" noWrap={true}>&nbsp; Stranger Friendly</Typography>}
-                </Stack>
-              </Stack>
-            <br></br>
-            {/* </div> */}
-            {/* <Col> */}
-            {/* <div> */}
-              <Typography component="legend" noWrap={true}>Playfulness</Typography>
-              <Rating value={playfulness} readOnly icon={<FilledBone fontSize="inherit" />}
-                emptyIcon={<EmptyBone fontSize="inherit" />} />
-              <br></br>
-              <Typography component="legend" noWrap={true}>Energy Levels</Typography>
-              <Rating value={energyLevels} readOnly icon={<FilledBone fontSize="inherit" />}
-                emptyIcon={<EmptyBone fontSize="inherit" />} />
-              <br></br>
-              <Typography component="legend" noWrap={true}>Exercise Needs</Typography>
-              <Rating value={exerciseNeeds} readOnly icon={<FilledBone fontSize="inherit" />}
-                emptyIcon={<EmptyBone fontSize="inherit" />} />
-              <br></br>
-            {/* </div> */}
-            {/* </Col> */}
-            </Col>
-          </Row>
-                </Box>
-                  </TabPanel>
+                  <Box>
+                    <Row className='attributes'>
+                      <Col>
+                        <Stack>
+                          <Stack direction="horizontal">
+                            {noviceFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
+                            {noviceFriendly && <Typography component="legend" noWrap={true}>&nbsp; Novice Friendly</Typography>}
+                          </Stack>
+
+                          <Stack direction='horizontal'>
+                            {kidFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
+                            {kidFriendly && <Typography component="legend" noWrap={true}>&nbsp; Kid Friendly</Typography>}
+                          </Stack>
+
+                          <Stack direction='horizontal'>
+                            {dogFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
+                            {dogFriendly && <Typography component="legend" noWrap={true}>&nbsp; Dog Friendly</Typography>}
+                          </Stack>
+
+                          <Stack direction='horizontal'>
+                            {catFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
+                            {catFriendly && <Typography component="legend" noWrap={true}>&nbsp; Cat Friendly</Typography>}
+                          </Stack>
+
+                          <Stack direction='horizontal'>
+                            {strangerFriendly && <RiCheckboxCircleFill color='#908AF8' fontSize="150%" />}
+                            {strangerFriendly && <Typography component="legend" noWrap={true}>&nbsp; Stranger Friendly</Typography>}
+                          </Stack>
+                        </Stack>
+                        <br></br>
+                        {/* </div> */}
+                        {/* <Col> */}
+                        {/* <div> */}
+                        <Typography component="legend" noWrap={true}>Playfulness</Typography>
+                        <Rating value={playfulness} readOnly icon={<FilledBone fontSize="inherit" />}
+                          emptyIcon={<EmptyBone fontSize="inherit" />} />
+                        <br></br>
+                        <Typography component="legend" noWrap={true}>Energy Levels</Typography>
+                        <Rating value={energyLevels} readOnly icon={<FilledBone fontSize="inherit" />}
+                          emptyIcon={<EmptyBone fontSize="inherit" />} />
+                        <br></br>
+                        <Typography component="legend" noWrap={true}>Exercise Needs</Typography>
+                        <Rating value={exerciseNeeds} readOnly icon={<FilledBone fontSize="inherit" />}
+                          emptyIcon={<EmptyBone fontSize="inherit" />} />
+                        <br></br>
+                        {/* </div> */}
+                        {/* </Col> */}
+                      </Col>
+                    </Row>
+                  </Box>
+                </TabPanel>
                 <TabPanel value='2'>
-                <Row className='shelter-loc'>
-                  {/* <h2>You can meet me at...</h2> */}
-                  <h4 className='detail-title'>Shelter</h4>
-                  <p>{dogInfo.shelter_name}</p>
-                  <h4 className='detail-title'>Address</h4>
-                  <p>{dogInfo.address}, {dogInfo.city}, {dogInfo.state} {dogInfo.zipcode}</p>
-                  <h4 className='detail-title'>Email</h4>
-                  <p>{dogInfo.email}</p>
-                </Row>
+                  <Row className='shelter-loc'>
+                    {/* <h2>You can meet me at...</h2> */}
+                    <h4 className='detail-title'>Shelter</h4>
+                    <p>{dogInfo.shelter_name}</p>
+                    <h4 className='detail-title'>Address</h4>
+                    <p>{dogInfo.address}, {dogInfo.city}, {dogInfo.state} {dogInfo.zipcode}</p>
+                    <h4 className='detail-title'>Email</h4>
+                    <p>{dogInfo.email}</p>
+                  </Row>
                 </TabPanel>
                 <TabPanel value='3'>
-                <Row>
-                  <Container >
-                    <div className="rounded me-auto shelter-map">
-                      <ShelterMap lat={dogInfo.latitude} lon={dogInfo.longitude} />
-                    </div>
-                  </Container>
-               </Row>
+                  <Row>
+                    <Container >
+                      <div className="me-auto shelter-map">
+                        <ShelterMap lat={dogInfo.latitude} lon={dogInfo.longitude} />
+                      </div>
+                    </Container>
+                  </Row>
                 </TabPanel>
-                </TabContext>
-                </Box>
-      </Row>
-     
-      <Row>
-        <div className='about-section'>
-
-          {/* <Row className='desc-1'>
-            <h2>I'm known for being...</h2>
-            <p>{dogInfo.desc_1}</p>
+              </TabContext>
+            </Box>
           </Row>
-          <Row className='desc-2'>
-            <h2>I'm looking for someone who...</h2>
-            <p>{dogInfo.desc_2}</p>
-          </Row> */}
-          
-         
-        </div>
-      </Row>
-    </Col>
+
+        </Col>
       </Row >
     </Container >
   )
@@ -245,23 +230,23 @@ export function DogProfile() {
 
 
 export function LoginModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">Login to submit an Adoption Inquiry</Modal.Title>
-        </Modal.Header>
-        {/* modal body is the form to login the user */}
-        <Modal.Body>
-            <LoginForm showLoginModal={props.showLoginModal}  userLoggedIn={props.userLoggedIn} setUserLoggedIn={props.setUserLoggedIn} onHide={props.onHide} />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button style={{color:'white'}} onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    )
-  }
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Login to submit an Adoption Inquiry</Modal.Title>
+      </Modal.Header>
+      {/* modal body is the form to login the user */}
+      <Modal.Body>
+        <LoginForm showLoginModal={props.showLoginModal} userLoggedIn={props.userLoggedIn} setUserLoggedIn={props.setUserLoggedIn} onHide={props.onHide} />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button style={{ color: 'white' }} onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
