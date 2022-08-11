9d95de359c8cd5f0a39aa80a208be2a5293039ce
Annesa Tran <annesat2020@gmail.com>
2022-08-10 18:27:17 -0700

    Created new custom BaltoToast component and context so toasts can appear across pages

---
+++ balto-ui/src/components/BaltoToast/BaltoToast.jsx
@@ -0,0 +1,18 @@
+import Toast from 'react-bootstrap/Toast'
+import DogIcon from '../../assets/paw icon.png'
+
+export default function BaltoToast({ show=false, setShow=()=>{}, header="Balto", body=""}) {
+    
+    return (
+      <div aria-live="polite" aria-atomic="true">
+        <Toast onClose={()=>setShow(false)} show={show} delay={3000} autohide className='update-toast' >
+          <Toast.Header>
+            <img src={DogIcon} className="dog-icon-toast" alt="dog paw"/>
+            <strong className="me-auto">{header}</strong>
+            <small className="text-muted">just now</small>
+          </Toast.Header>
+          <Toast.Body>{body}</Toast.Body>
+        </Toast>
+      </div>
+    );
+  }
